d28c4f643fbf14ed5d5e88b6658846af675c6915
adamga <adamga@microsoft.com>
2025-04-09 08:30:58 -0400

    Update binaries and assembly information for GolfPoolApp, reflecting changes in build output and sourcelink configuration.

--- GolfPoolApp/obj/Debug/net9.0/GolfPoolApp.AssemblyInfo.cs
+++ GolfPoolApp/obj/Debug/net9.0/GolfPoolApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolfPoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc913e05a656a1aae69f2c478be9909699137a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b60ce48104496e4397ddce3b95583648afc8aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolfPoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolfPoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
